5cc6c9893cb7ee9723e41eeb5c8d5eb580a3feea
miguel aguilar <maguilarv@unprg.edu.pe>
2019-11-15 19:59:34 -0500

    se agrego el ejercicio 5 de condicional simple

---
+++ simples/ejercicio05.py
@@ -0,0 +1,41 @@
+import os
+#Boleta de venta de bebidas
+#declarar variables
+cliente,producto,six_pack,pu="","",0,0.0
+comprador_compulsivo=False
+
+#INPUT
+cliente=os.sys.argv[1]
+producto=os.sys.argv[2]
+six_pack=int(os.sys.argv[3])
+pu=float(os.sys.argv[4])
+
+#PROCESSING
+total = (six_pack * pu)
+descuento = (six_pack*0.5)
+precio_a_pagar = (total-descuento)
+
+#VERIFICADOR
+comprador_compulsivo=(precio_a_pagar>3000)
+
+#OUTPUT
+print("#############################")
+print("#           METRO          #")
+print("#############################")
+print("#")
+print("# Cliente          : ", cliente)
+print("# Producto         : ", producto)
+print("# Item             : ", six_pack, "six pack")
+print("# P.U.             : ", pu)
+print("# Total            : ", total)
+print("# Descuento        : ", descuento)
+print("# Precio a pagar   : ", precio_a_pagar)
+print("#############################")
+print("compra excesiva?:", comprador_compulsivo)
+
+#condicional simple
+#si el comprador es compulsivo mostras que ha ganado un viaje a ICA
+if(comprador_compulsivo==True):
+    print("OFERTA: usted ha ganado un viaje a Ica todo pagado")
+
+#fin_if

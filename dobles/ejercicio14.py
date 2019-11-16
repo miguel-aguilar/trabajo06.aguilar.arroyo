d69d8b6285dcdffb9aac1a7195b3f5c0a118cf76
miguel aguilar <maguilarv@unprg.edu.pe>
2019-11-15 20:09:31 -0500

    se agrego el ejercicio 14 de condicional doble

---
+++ dobles/ejercicio14.py
@@ -0,0 +1,43 @@
+import os
+#Boleta de venta de GOLOSINAS
+#declarar variables
+cliente,producto,bolsas,pu="","",0,0.0
+comprador_excesivo=False
+#INPUT
+cliente=os.sys.argv[1]
+producto=os.sys.argv[2]
+bolsas=int(os.sys.argv[3])
+pu=float(os.sys.argv[4])
+
+#PROCESSING
+total = (bolsas * pu)
+descuento = (bolsas*0.3)
+precio_a_pagar = (total-descuento)
+
+#VERIFICADOR
+comprador_excesivo=(precio_a_pagar>250)
+
+#OUTPUT
+print("#############################")
+print("# BOLETA DE VENTA")
+print("#############################")
+print("#")
+print("# Cliente          : ", cliente)
+print("# Producto         : ", producto)
+print("# Item             : ", bolsas, "bolsas")
+print("# P.U.             : ", pu)
+print("# Total            : ", total)
+print("# Descuento        : ", descuento)
+print("# Precio a pagar   : ", precio_a_pagar)
+print("#############################")
+print("comprador compulsivo?:", comprador_excesivo)
+
+#condicional doble
+#si el comprador es cpompulsivo mostrarle que ha ganado 10 bolsas mas del producto
+#si el comprador no es compulsivo decirle que siga intentado
+if(comprador_excesivo==True):
+    print("OFERTA: usted ha ganado 10 bolsas de " + producto + " mas")
+
+else:
+    print("no pudo ganar nada, pero siga intentando")
+#fin_if
